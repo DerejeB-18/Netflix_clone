0346a1686df7ecbb3333880db2c4d202edfec3a2
nafolbb <viivaanbedada@gmail.com>
2026-01-19 12:35:49 +0300

    header and footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Home from './pages/Home/home';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Header />  {/* Displays ONCE at the top */}
+      <Home />    {/* Displays your page content */}
+      <Footer />  {/* Displays ONCE at the bottom */}
     </div>
   );
 }
---
+++ netflix-clone/src/pages/Home/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./home.css"; 
+
+const Home = () => {
+  return (
+    <div className="home">
+        {/* {/* DO NOT PUT <Header /> or <Footer /> HERE. 
+            They are already coming from App.js 
+        */}
+        <div style={{height: "500px", color: "white", padding: "100px"}}>
+            {/* <h1>Main Home Content (Banner/Rows) will go here</h1> */}
+        </div>
+    </div>
+  )
+}
+
+export default Home;
